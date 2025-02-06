f86e5c08256bbecd5a15b283f97b4416dfc879dc
Mateusz Pepla <matipejpal@gmail.com>
2025-02-06 16:18:33 +0100

    Initial commit of the project

---
+++ app/HtmlTree.hs
@@ -0,0 +1,83 @@
+module HtmlTree where
+
+import Control.Arrow ((&&&), Arrow (second))
+import Data.Char (isSpace)
+import Data.List.Split
+
+-- to do:
+-- turn uses of head and tail into safe head and tail
+-- check for other empty list errors
+-- delete Text nodes with only white space
+
+data HtmlTree
+    = Text { content :: String }
+    | HtmlTag 
+     { htmltagType :: String
+     , htmlAttrs :: [TagAttr]
+     , htmlchildren :: [HtmlTree]}
+    deriving (Show, Eq)
+
+data TagAttr = TagAttr
+    { attrName :: String
+    , attrVal :: String}
+    deriving (Show, Eq)
+
+-- takes html text, tagType accumulator
+-- returns tuple of (tagType, remaining html text, whether SelfClosing)
+parseTagType :: String -> String -> (String, String, Bool, [TagAttr])
+parseTagType [] tagType = ([], tagType, True, [])
+parseTagType (x:xs) tagType
+    | x == '/' = 
+        if head xs == '>' 
+        then let
+        (tag, attrs) = parseAttr tagType
+        in (tag, tail xs, True, attrs)
+        else parseTagType xs $ tagType ++ [x]
+    | x == '>' = let
+        (tag, attrs) = parseAttr tagType
+        in (tag, xs, False, attrs)
+    | otherwise = parseTagType xs $ tagType ++ [x]
+
+-- takes whole tag
+-- returns tagType, list of attributes
+parseAttr :: String -> (String, [TagAttr])
+parseAttr = (fst &&& parseAttrList . splitOn "\"" . snd) . break isSpace
+
+parseAttrList :: [String] -> [TagAttr]
+parseAttrList [] = []
+parseAttrList [_] = []
+parseAttrList (x:y:zs) = TagAttr (removeFirstLast x) y : parseAttrList zs
+
+removeFirstLast :: String -> String
+removeFirstLast = tail . init 
+
+-- takes html text, children accumulator, text accumulator
+-- returns a tuple of (list of children, remaining html text)
+parseText :: String -> [HtmlTree] -> String -> ([HtmlTree], String)
+parseText [] children _ = (children, [])
+parseText (x:xs) children text 
+    | x == '<' = if head xs == '/'
+        then (children ++ [Text text | not (all isSpace text)], removeTag xs)
+        else let
+            (child, remaining) = parseTag xs
+            in parseText remaining (children++[child]) ""
+    | otherwise = parseText xs children (text++[x])
+
+parseTag :: String -> (HtmlTree, String)
+parseTag xs
+    | head xs `elem` ['!', '/'] = parseHTML $ removeTag xs
+    | otherwise = (HtmlTag tagType attrs children, remaining2)
+  where
+    (tagType, remaining, selfClose, attrs) = parseTagType xs []
+    (children, remaining2) = if selfClose then ([], remaining) else parseText remaining [] ""
+
+-- removes everything until end of tag
+removeTag :: String -> String
+removeTag = drop 1 . dropWhile (/= '>')
+
+parseHTML :: String -> (HtmlTree, String)
+parseHTML = parseTag . drop 1 . dropWhile (/= '<')
+
+createTree :: String -> HtmlTree
+createTree = fst . parseHTML . removeTag
+
---
+++ app/Main.hs
@@ -0,0 +1,53 @@
+module Main where
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Network.HTTP.Simple as S
+import Network.HTTP.Client.TLS 
+import Network.HTTP.Client
+import qualified Data.ByteString.Lazy.Char8 as L8
+import System.Console.ANSI (setSGR, SGR(..), Color(..), ColorIntensity(..), ConsoleLayer(Foreground))
+import System.IO (hFlush, stdout)
+import HtmlTree
+
+-- Function to format and display text in the terminal
+displayText :: String -> IO ()
+displayText text = do
+    setSGR [SetColor Foreground Vivid Blue]  -- Set text color to blue
+    putStrLn "=== Extracted Text ==="
+    setSGR [Reset]  -- Reset text color
+    putStrLn text
+
+-- Function that prints html tree
+indent :: Int -> String
+indent n = replicate (n * 2) ' '
+
+printAttr :: TagAttr -> String
+printAttr (TagAttr name val) = name ++ "=\"" ++ val ++ "\""
+
+printAttrs :: [TagAttr] -> String
+printAttrs attrs = if null attrs then "" else " " ++ unwords (map printAttr attrs)
+
+printHtmlTree :: Int -> HtmlTree -> String
+printHtmlTree level (Text content) = indent level ++ "Text: " ++ content ++ "\n"
+printHtmlTree level (HtmlTag tagType attrs children) =
+    let openingTag = tagType ++ printAttrs attrs
+        childrenStr = concatMap (printHtmlTree (level + 1)) children
+    in indent level ++ openingTag ++ "\n" ++ childrenStr ++ indent level ++ "\n"
+
+printHtml :: HtmlTree -> IO ()
+printHtml tree = putStr (printHtmlTree 0 tree)
+
+main :: IO ()
+main = do
+    putStr "Enter website URL (include http:// or https://): "
+    hFlush stdout
+    url <- getLine
+
+    -- Fetch HTML content
+    manager <- newManager tlsManagerSettings
+    request <- parseRequest url
+    response <- httpLbs request manager
+    let html = L8.unpack $ S.getResponseBody response
+    putStrLn "=== Tree ==="
+    printHtml $ createTree html
+
